8f48736e4bac0909a36aff29031a267ba5e03e60
VeritaxDR <rappaniagua>
2025-04-07 01:13:14 -0400

    Suma dos números complejos. Define dos números complejos y muestra el resultado de su suma.

---
+++ Ejercicio 3 practica 1.py
@@ -0,0 +1,7 @@
+#Suma dos números complejos. Define dos números complejos y muestra el
+#resultado de su suma.
+num1 = complex(2, 3)  # Número complejo 1 (2 + 3i)
+print("Número complejo 1:", num1)
+num2 = complex(4, 5)  # Número complejo 2 (4 + 5i)
+print("Número complejo 2:", num2)
+suma = num1 + num2  # Suma de los números complejos
