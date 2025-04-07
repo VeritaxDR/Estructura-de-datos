48b4d71248c74895a0bdca5d754499ca1c946a14
VeritaxDR <rappaniagua>
2025-04-07 01:08:15 -0400

    Convierte grados Celsius a Fahrenheit. Pide al usuario que ingrese la temperatura en Celsius y muestra el resultado en Fahrenheit.

---
+++ Ejercicio 2 practica 1.py
@@ -0,0 +1,6 @@
+#Convierte grados Celsius a Fahrenheit. Pide al usuario que ingrese la temperatura en Celsius y muestra el resultado en Fahrenheit.
+Celsius=float(input("Ingrese la temperatura en Celsius: "))
+Fahrenheit=(Celsius*9/5)+32
+
+#Resultado impreso en pantalla.
+print("La temperatura en Fahrenheit es:", Fahrenheit)
