257c6596530c7bc5f547ac0b8f99a5ee97291b6d
VeritaxDR <rappaniagua>
2025-04-07 01:32:25 -0400

    Pide al usuario que ingrese su edad y determina si es mayor de edad (18 años o más).

---
+++ practica 1/Ejercicio 5 practica 1.py
@@ -0,0 +1,6 @@
+#Pide al usuario que ingrese su edad y determina si es mayor de edad (18 años o más).
+edad = int(input("ingrese su edad:"))
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
