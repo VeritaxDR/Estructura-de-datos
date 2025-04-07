d91bcf3696635bbf62cfc04be85dc69417803d93
VeritaxDR <rappaniagua>
2025-04-07 01:00:10 -0400

    Calcula el área de un rectángulo. Pide al usuario que ingrese el largo y el ancho, y luego muestra el área.

---
+++ Ejercicio 1 practica 1.py
@@ -0,0 +1,7 @@
+#Calcula el área de un rectángulo. Pide al usuario que ingrese el largo y el ancho, y luego muestra el área.
+alto=float(input("Ingrese el alto del rectángulo: "))
+ancho=float(input("Ingrese el ancho del rectángulo: "))
+
+#Calculo del area.
+area=alto*ancho
+print("El área del rectángulo es:", area)
