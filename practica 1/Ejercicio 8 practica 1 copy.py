9cee9f0bfc9dcdf8cb146817dc9594bc9d62c98d
VeritaxDR <rappaniagua>
2025-04-13 22:46:40 -0400

    actualizacion

--- practica 1/Ejercicio 8 practica 1 copy.py
+++
@@ -1,6 +0,0 @@
-#Escribe un programa que pida al usuario un número y determine si es par o impar. 
-num1 = int(input("Introduce un número: "))
-if num1 % 2 == 0:
-    print("El número es par.")
-else:
-    print("El número es impar.")
